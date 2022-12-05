f0a3638c9f6a0e3a60f2891a1b4658adf20524bd
William Chang <william1007.us@gmail.com>
2022-12-05 17:40:28 +0000

    forum updated

--- src/pages/Forums.js
+++ src/pages/Forums.js
@@ -1,12 +1,13 @@
-import { Container, Grid } from "@mui/material";
-import React,{ useEffect, useState }  from "react";
+import { Container, Grid, Box, Button, InputAdornment, TextField } from "@mui/material";
+import { Search } from '@mui/icons-material';
+import React, { useEffect, useState } from "react";
 import QuestionList from "../component/QuestionList";
 import SideBar from "../component/SideBar";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import {db} from '../firebaseSetup/firebase';
-import {collection, query, orderBy, onSnapshot, getCountFromServer, doc, getDoc} from "firebase/firestore";
+import { db } from '../firebaseSetup/firebase';
+import { collection, query, orderBy, onSnapshot, getCountFromServer, doc, getDoc } from "firebase/firestore";
 // import firebase from "firebase";
-import {Pagination} from 'react-bootstrap';
+import { Pagination } from 'react-bootstrap';
 
 const theme = createTheme({
     palette: {
@@ -23,7 +24,7 @@ export default function Forum() {
     const [questions, setQuestions] = useState([]);
 
     /** function to get all questions from firestore in realtime **/
-    useEffect(()=>{
+    useEffect(() => {
         const questionColRef = query(collection(db, 'questions'), orderBy('created', 'desc'))
         onSnapshot(questionColRef, (snapshot) => {
             setQuestions(snapshot.docs.map(doc => ({
@@ -31,20 +32,44 @@ export default function Forum() {
                 data: doc.data()
             })))
         })
-   
-    },[])
+
+    }, [])
 
     return (
         <ThemeProvider theme={theme}>
-            <Container style={{marginTop:'2rem', marginBottom:'10rem'}}>
-                <Grid container spacing={{xs:2, md:3}} columns={{xs:4, sm:8, md:12}}>
-                    <Grid item xs={3}>
-                        <SideBar />
+            <Container style={{ marginTop: '2rem', marginBottom: '10rem' }}>
+                <Grid container>
+                    <Grid item container xs={12} spacing={2} justifyContent="center">
+                            <Grid item xs={12} md={9}>
+                                <TextField
+                                    id="forum-search"
+                                    label="Search"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <Search />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    variant="outlined"
+                                    fullWidth
+                                    size='small'
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={3}>
+                                <Button variant="contained" fullWidth size='medium'>Search</Button>
+                            </Grid>
                     </Grid>
-                    <Grid item xs={9}>
-                        <QuestionList />
+                    <Grid item container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        <Grid item xs={3}>
+                            <SideBar />
+                        </Grid>
+                        <Grid item xs={9}>
+                            <QuestionList />
+                        </Grid>
                     </Grid>
                 </Grid>
+
             </Container>
         </ThemeProvider>
     );
